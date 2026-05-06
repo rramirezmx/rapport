c8404a1700845007e02ffbb4b0fc215b5c27b64a
Raul Ramirez <raul.ramirez@esceptica.mx>
2026-05-05 21:40:24 -0600

    Lines of 1-setup.sh are commented

--- 1-setup.sh
+++ 1-setup.sh
@@ -8,16 +8,16 @@ sudo mount -o size=512M -t tmpfs none sandbox
 
 
 # Required for routers-connectivity/frr testing (optional)
-sudo /usr/lib/frr/frrinit.sh start
+#sudo /usr/lib/frr/frrinit.sh start
 
 
 # Required for routers-connectivity/cisco testing (optional)
-sudo ip link add br-lab type bridge
-sudo ip addr add 10.0.0.1/24 dev br-lab
-sudo ip link set br-lab up
-sudo mkdir -p /etc/qemu
-echo "allow br-lab" | sudo tee -a /etc/qemu/bridge.conf > /dev/null
-sudo chmod 644 /etc/qemu/bridge.conf
-HELPER_PATH="/usr/lib/qemu/qemu-bridge-helper" 
-sudo chmod u+s $HELPER_PATH
-sudo ufw allow in on br-lab to any port 8323 proto tcp
+#sudo ip link add br-lab type bridge
+#sudo ip addr add 10.0.0.1/24 dev br-lab
+#sudo ip link set br-lab up
+#sudo mkdir -p /etc/qemu
+#echo "allow br-lab" | sudo tee -a /etc/qemu/bridge.conf > /dev/null
+#sudo chmod 644 /etc/qemu/bridge.conf
+#HELPER_PATH="/usr/lib/qemu/qemu-bridge-helper" 
+#sudo chmod u+s $HELPER_PATH
+#sudo ufw allow in on br-lab to any port 8323 proto tcp
